073cd8a4083cf3301ecfd6555b8aa6ea703c88b8
MichaelSudirman <astradon.michael@gmail.com>
2020-01-30 23:12:14 +0700

    added image storage and upload feature

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -1,4 +1,4 @@
-const { db } = require("../util/admin");
+const { admin, db } = require("../util/admin");
 
 const config = require("../util/config");
 
@@ -20,7 +20,8 @@ exports.signup = (req, res) => {
   const { valid, errors } = validateSignupData(newUser);
   if (!valid) return res.status(400).json(errors);
 
-  // TODO: validate data
+  const noImg = "no-img.png";
+  // Validate data
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
     .get()
@@ -52,6 +53,7 @@ exports.signup = (req, res) => {
         handle: newUser.handle,
         email: newUser.email,
         createdAt: new Date().toISOString(),
+        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
         userId
       };
       // return the response JWT token
@@ -80,8 +82,7 @@ exports.login = (req, res) => {
   // error handling
   const { valid, errors } = validateLoginData(user);
   if (!valid) return res.status(400).json(errors);
-  
-  
+
   firebase
     .auth()
     .signInWithEmailAndPassword(user.email, user.password)
@@ -100,4 +101,61 @@ exports.login = (req, res) => {
           .json({ general: "Wrong credentials, please try again" });
       } else return res.status(500).json({ error: err.code });
     });
+};
+
+exports.uploadImage = (req, res) => {
+  const BusBoy = require("busboy");
+  const path = require("path");
+  const os = require("os");
+  const fs = require("fs");
+
+  const busboy = new BusBoy({ headers: req.headers });
+
+  let imageFileName;
+  let imageToBeUploaded = {};
+
+  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+    console.log(fieldname, file, filename, encoding, mimetype);
+    // split the string per dot: my.image.png -> ['my','image','png']
+    const imageExtension = filename.split('.')[filename.split('.').length - 1]
+    // exampe: 1374574.png
+    imageFileName = `${Math.round(
+      Math.random() * 1000000000000
+    ).toString()}.${imageExtension}`;
+    const filepath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filepath, mimetype };
+    file.pipe(fs.createWriteStream(filepath));
+  });
+  console.log(imageFileName);
+  busboy.on("finish", () => {
+    admin
+      .storage()
+      .bucket()
+      .upload(imageToBeUploaded.filepath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUploaded.mimetype
+          }
+        }
+      })
+      .then(() => {
+        // alt=media option allows user to view instead of download the media
+        // url harcoded given from firebase docs
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+        return db
+          .doc(`/users/${req.user.handle}`)
+          .update({ imageUrl: imageUrl });
+      })
+      .then(() => {
+        return res.json({
+          message: "Image uploaded successfully"
+        });
+      })
+      .catch(err => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+      });
+  });
+  busboy.end(req.rawBody);
 };
--- functions/index.js
+++ functions/index.js
@@ -3,10 +3,10 @@ const functions = require("firebase-functions");
 // initialize express
 const app = require("express")();
 
-const FBAuth = require('./util/fbAuth')
+const FBAuth = require("./util/fbAuth");
 
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login } = require("./handlers/users");
+const { signup, login, uploadImage } = require("./handlers/users");
 
 // scream routes
 app.get("/screams", getAllScreams);
@@ -15,6 +15,7 @@ app.post("/scream", FBAuth, postOneScream);
 // users routes
 app.post("/signup", signup);
 app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
 
 // https://baseurl.com/api/scream <- /api/ is good practice
 // https://api.baseurl.com/scream <- good practice alternative
