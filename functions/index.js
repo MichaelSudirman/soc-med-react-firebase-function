16dd4b4a89320136d61afaa1a490d6c3a164058d
MichaelSudirman <astradon.michael@gmail.com>
2020-01-30 11:58:40 +0700

    added signup feature that add authentication id

--- functions/index.js
+++ functions/index.js
@@ -4,15 +4,32 @@ const admin = require('firebase-admin')
 // project already know the id from .firebaserc
 admin.initializeApp();
 
+
 // initialize express and app
 const express = require('express');
 const app = express();
 
+// initialize firebase
+const firebase = require('firebase');
+const firebaseConfig = {
+    apiKey: "AIzaSyApgLjRjuC6L1bbLP7BWT1RXq2oucZ1q5g",
+    authDomain: "soc-med.firebaseapp.com",
+    databaseURL: "https://soc-med.firebaseio.com",
+    projectId: "soc-med",
+    storageBucket: "soc-med.appspot.com",
+    messagingSenderId: "378654173438",
+    appId: "1:378654173438:web:c0a760f8d37bb775f15daf",
+    measurementId: "G-9V75C90226"
+};
+firebase.initializeApp(firebaseConfig)
+
+// rename db for clean code
+const db = admin.firestore();
+
 app.get('/screams', (req, res) => {
-    admin
-        .firestore()
+    db
         .collection('screams')
-        .orderBy('createdAt','desc')
+        .orderBy('createdAt', 'desc')
         .get()
         .then(data => {
             let screams = [];
@@ -23,7 +40,7 @@ app.get('/screams', (req, res) => {
                     // rewrite all 3 data
                     body: doc.data().body,
                     userHandle: doc.data().userHandle,
-                    createdAt:doc.data().createdAt
+                    createdAt: doc.data().createdAt
                 });
             });
             return res.json(screams);
@@ -43,8 +60,7 @@ app.post('/scream', (req, res) => {
         createdAt: new Date().toISOString()
     };
 
-    admin
-        .firestore()
+    db
         .collection('screams')
         .add(newScream)
         .then(doc => {
@@ -57,7 +73,37 @@ app.post('/scream', (req, res) => {
         })
 })
 
+// Signup route
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    }
+
+    // TODO validate data
+
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then(data => {
+            // status 201 = resource created
+            return res
+                .status(201)
+                .json({
+                    message: `user ${data.user.uid} signed up successfully`
+                })
+                .catch(err => {
+                    console.error(err);
+                    // status 500 = server error
+                    return res.status(500).json({
+                        error: err.code
+                    })
+                })
+        })
+})
+
 // https://baseurl.com/api/scream <- /api/ is good practice
 // https://api.baseurl.com/scream <- good practice alternative
-
 exports.api = functions.https.onRequest(app);
