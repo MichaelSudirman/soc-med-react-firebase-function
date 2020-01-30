cbc9a847ef60d47b451c31cb49a4f19f740dcc09
MichaelSudirman <astradon.michael@gmail.com>
2020-01-30 14:17:36 +0700

    added validation to sign up feature

--- functions/index.js
+++ functions/index.js
@@ -70,6 +70,17 @@ app.post("/scream", (req, res) => {
     });
 });
 
+// helper function for checking empty and is email
+const isEmpty = string => {
+  if (string.trim() === "") return true;
+  else return false;
+};
+const isEmail = email => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
 // Signup route
 app.post("/signup", (req, res) => {
   // newUser will be stored in firebase authentication
@@ -79,6 +90,20 @@ app.post("/signup", (req, res) => {
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle
   };
+
+  // error handling
+  let errors = {};
+  if (isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty';
+  } else if (!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address';
+  }
+
+  if(isEmpty(newUser.password)) errors.password = 'Must not be empty';
+  if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+  if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+  if(Object.keys(errors).length > 0)return res.status(400).json(errors)
 
   // TODO: validate data
   let token, userId;
