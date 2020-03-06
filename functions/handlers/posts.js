9d93d7cef5fbc0d8badc560707d540a214fafd06
MichaelSudirman <astradon.michael@gmail.com>
2020-03-06 17:13:42 +1000

    fix json content for comment exception handling

--- functions/handlers/posts.js
+++ functions/handlers/posts.js
@@ -86,7 +86,7 @@ exports.getPost = (req, res) => {
 // comment on a post
 exports.commentOnPost = (req, res) => {
   if (req.body.body.trim() === "")
-    return res.status(400).json({ error: "Must not be empty" });
+    return res.status(400).json({ comment: "Must not be empty" });
 
   const newComment = {
     body: req.body.body,
